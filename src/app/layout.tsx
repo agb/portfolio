d84736cabfa5ef0b2a22b144b56299984a4523ab
Ali Bektash <agokselb@gmail.com>
2026-01-04 18:25:18 +0900

    fix: :pencil2: Update Google verification code in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
   },
   verification: {
-    google: "",
+    google: "RMGt4UQEeb0zc5xlqTT46BTzVrE20j3gQMbnpuy-aZg",
     yandex: "",
   },
 };
